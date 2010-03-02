7baa1ff8bcabed30c574c44a2bfab9869233dc96
Karl Smith <threadhead@gmail.com>
2010-03-02 14:34:26 -0700

    adjust test script

--- test.rb
+++ test.rb
@@ -8,4 +8,9 @@ sp.open
 sp.add_thermostat(1)
 sp.get_thermostat(1).get_model
 
-puts "#{sp.get_thermostat(1).registers.model_of_thermostat}"
+puts "model: #{sp.get_thermostat(1).registers.model_of_thermostat}"
+
+sp.get_thermostat(1).get_limits
+
+puts "cool_setpoint_low_limit: #{sp.get_thermostat(1).registers.cool_setpoint_low_limit}"
+puts "heat_setpoint_high_limit: #{sp.get_thermostat(1).registers.heat_setpoint_high_limit}"
