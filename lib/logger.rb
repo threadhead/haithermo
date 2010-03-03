6a3cb453bad41b0d87241737a8a3a0fbb5e6edd3
Karl Smith <threadhead@gmail.com>
2010-03-03 13:50:05 -0700

    work on logger

--- lib/logger.rb
+++ lib/logger.rb
@@ -7,24 +7,26 @@ module HAIthermo
     def self.new(file, level = Logger::DEBUG)
       if file
         HAIthermo.logger = Logger.new(log_file)
-        HAIthermo.logger.level = level
-        HAIthermo.logger.datetime_format = "%d %b %H:%M:%S"
-        HAIthermo.logger.formatter = proc { |severity, datetime, progname, msg|
-            "[#{datetime}] #{msg}\n"
-          }
+      else
+        HAIthermo.logger = Logger.new(STDOUT)
       end
+      HAIthermo.logger.level = level
+      HAIthermo.logger.datetime_format = "%d %b %H:%M:%S"
+      HAIthermo.logger.formatter = proc { |severity, datetime, progname, msg|
+          "[#{datetime}] #{msg}\n"
+        }
     end
     
-    def info(message)
-      HAIthermo.logger.info message 
-    end
-    
-    def debug(message)
-      HAIthermo.logger.debug message
-    end
-    
-    def close
-      HAIthermo.logger.close
-    end
+    # def info(message)
+    #   HAIthermo.logger.info message 
+    # end
+    # 
+    # def debug(message)
+    #   HAIthermo.logger.debug message
+    # end
+    # 
+    # def close
+    #   HAIthermo.logger.close
+    # end
   end
 end
