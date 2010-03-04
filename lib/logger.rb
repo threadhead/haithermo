bfeae422466b1cbaeb213600416f770016ddd05c
Karl Smith <threadhead@gmail.com>
2010-03-03 17:08:16 -0700

    work on logger

--- lib/logger.rb
+++ lib/logger.rb
@@ -14,20 +14,8 @@ module HAIthermo
       logger.formatter = proc { |severity, datetime, progname, msg|
           "[#{datetime.strftime("%d %b %H:%M:%S.%2N")}] #{msg}\n"
         }
-        puts "logger format: #{logger.datetime_format}"
       return logger
     end
-    
-    # def info(message)
-    #   HAIthermo.logger.info message 
-    # end
-    # 
-    # def debug(message)
-    #   HAIthermo.logger.debug message
-    # end
-    # 
-    # def close
-    #   HAIthermo.logger.close
-    # end
+
   end
 end
